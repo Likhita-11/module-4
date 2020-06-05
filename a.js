867e083c735f851c4259fa33a27598b7693a47d2
Likhita-11 <62496768+Likhita-11@users.noreply.github.com>
2020-06-05 12:19:33 +0530

    Create a.js

---
+++ a.js
@@ -0,0 +1,11 @@
+var arrayNames =['Ajay','Niranjan','Likitha','siddu','japan','india','jaggu','Australia','jambu','jpriya','darliki'];
+for (var i =0;i<arrayNames.length; i++) {
+var name = arrayNames[i];
+if(name.startsWith('j') || name.startsWith('J')) {
+console.log(' GoodBye ' + 'J' + name);
+}
+else {
+console.log(' Hello ' + name);
+}
+}
+
